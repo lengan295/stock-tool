b90b0d618f1999d23539d7027f2e23b67deb9011
Le Ngan <lengan93ha@gmail.com>
2021-06-20 11:31:17 +0700

    doctrine : cli-config.php

---
+++ cli-config.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new \DI\ContainerBuilder();
+
+// Set up settings
+$settings = require __DIR__ . '/app/settings.php';
+$settings($containerBuilder);
+
+$container = $containerBuilder->build();
+/** @var \App\Application\Settings\SettingsInterface $settings */
+$settings = $container->get(\App\Application\Settings\SettingsInterface::class);
+
+$paths = array("src");
+$isDevMode = true;
+$dbParams = $settings->get('database');
+
+$config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
+$entityManager = \Doctrine\ORM\EntityManager::create($dbParams, $config);
+
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+
