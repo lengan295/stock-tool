a4868b3579855a6ed8f929a2f03b2017447aab4c
Le Ngan <lengan93ha@gmail.com>
2021-06-23 16:19:19 +0700

    import company by industry

--- app/routes.php
+++ app/routes.php
@@ -16,7 +16,7 @@ return function (App $app) {
 
     $app->get('/', \App\Application\Actions\Homepage\HomepageAction::class);
 
-    $app->get('/import', \App\Application\Actions\Company\ImportAction::class);
+    $app->get('/import/{industryCode}', \App\Application\Actions\Company\ImportAction::class);
 
     $app->get('/analyse', \App\Application\Actions\Company\AnalyseAction::class);
 
