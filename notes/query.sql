c915bf4c89b3c68576d081354ec39869ab0fe0e6
Le Ngan <lengan93ha@gmail.com>
2021-07-08 19:41:44 +0700

    notes

--- notes/query.sql
+++ notes/query.sql
@@ -1,7 +1,11 @@
-SELECT c.code, c.code_industry, c.roic,
-    ca4m.nopat_grow_rate,
-    ca4m.net_income_grow_rate,
-    ca4m.equity_grow_rate,
+-- filtered list : good price
+SELECT c.code,
+       c.code_industry,
+       c.market_cap,
+       c.roic,
+       ca4m.nopat_grow_rate,
+       ca4m.net_income_grow_rate,
+       ca4m.equity_grow_rate,
        ca4m.operating_cash_flow_grow_rate,
        c.stock_price,
        ca4m.mos_price,
@@ -14,6 +18,21 @@ WHERE c.stock_price <= ca4m.chosen_price + 2000
   AND ca4m.net_income_grow_rate >= 0.1
   AND ca4m.equity_grow_rate >= 0.1
 #   AND ca4m.operating_cash_flow_grow_rate >= 0.1
-#   AND c.code_industry = 3763
+  AND c.code_industry = 3500
 ;
 
+-- specified company
+SELECT c.code,
+       c.code_industry,
+       c.market_cap,
+       c.roic,
+       ca4m.nopat_grow_rate,
+       ca4m.net_income_grow_rate,
+       ca4m.equity_grow_rate,
+       ca4m.operating_cash_flow_grow_rate,
+       c.stock_price,
+       ca4m.mos_price,
+       ca4m.graham_mos_price
+FROM company c
+         JOIN company_analysing_4m ca4m on c.code = ca4m.code
+WHERE c.code = 'VNM';
